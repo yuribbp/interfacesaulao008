c13519de0f42290412ae7bc182a874d017bf9f2f
Yuri Baldan <yuribbp@gmail.com>
2020-09-23 17:07:23 -0300

    Finalizado

--- src/entities/Installment.java
+++ src/entities/Installment.java
@@ -1,9 +1,11 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Installment {
 
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	private Date dueDate;
 	private Double amount;
 	
@@ -30,6 +32,11 @@ public class Installment {
 
 	public void setAmount(Double amount) {
 		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return sdf.format(dueDate) + " - " + String.format("%.2f", amount);
 	}
 	
 }
